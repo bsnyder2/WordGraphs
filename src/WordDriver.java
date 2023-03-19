0ba7cd1fc5b5ff78780d07e6adb94eaaeab12d80
Ben Snyder <119763240+benv2s@users.noreply.github.com>
2023-03-19 17:34:19 -0400

    Added test code

--- src/WordDriver.java
+++ src/WordDriver.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class WordDriver {
     /**
      * Loads all words from a given file into a HashSet.
+     * 
      * @return validWords
      */
     private static HashSet<String> loadWords(String filename) {
@@ -30,8 +31,16 @@ public class WordDriver {
     public static void main(String[] args) {
         String filename = "../wordsets/words-58k.txt";
         HashSet<String> validWords = loadWords(filename);
-        
-        WordGraph g = new WordGraph(validWords);
-        WordTrace t = new WordTrace(g);
+
+        boolean[] ops = { true, false, true };
+        WordGraph g = new WordGraph(validWords, ops);
+
+        String startWord = "treats";
+        String[] words = { "tea", "rest", "reasons", "science" };
+        for (int i = 0; i < words.length; i++) {
+            WordTrace t = new WordTrace(g, startWord, words[i]);
+            t.BFS();
+            System.out.println(t.toString() + "\n");
+        }
     }
 }
