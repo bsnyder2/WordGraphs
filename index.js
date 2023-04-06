5f3784a62495cfb6399746cdb4b3651a46baaec2
Ben Snyder <119763240+benv2s@users.noreply.github.com>
2023-04-06 15:55:15 -0400

    Cleaned up control flow, variable names

--- index.js
+++ index.js
@@ -23,6 +23,104 @@ async function loadWords(filename) {
     return new Set(text.split("\n"));
 }
 
+async function submitClick(g) {
+    // Read in words from input fields
+    const startWord = document.getElementById("start").value.trim().toLowerCase();
+    const endWord = document.getElementById("end").value.trim().toLowerCase();
+
+    // Hide output and clear trace
+    const outputbox = document.getElementById("outputbox");
+    outputbox.hidden = true;
+    const tracebox = document.getElementById("tracebox");
+    tracebox.innerHTML = "";
+
+    // Display goal path
+    const goal = document.getElementById("goal");
+    goal.innerText = `${startWord} \u2192 ${endWord}`;
+
+    // Check for new operations
+    const newops = [false, false, false];
+    if (document.getElementById("set").checked) {
+        newops[0] = true;
+    }
+    if (document.getElementById("add").checked) {
+        newops[1] = true;
+    }
+    if (document.getElementById("remove").checked) {
+        newops[2] = true;
+    }
+
+    // If nothing in map, or operations have changed
+    let needsRebuild = false;
+    if (g.map.size == 0 || g.ops.toString() != newops.toString()) {
+        needsRebuild = true;
+        g.ops = newops;
+    }
+
+    // Check for input errors (see docs)
+    if (!g.validWords.has(startWord)) {
+        logResult(2, null);
+        return g;
+    }
+    if (!g.validWords.has(endWord)) {
+        logResult(3, null);
+        return g;
+    }
+    startLen = startWord.length;
+    endLen = endWord.length;
+    if (!((startWord == endWord) ||
+        ((startLen == endLen) && (g.ops[0] || (g.ops[1] && g.ops[2]))) ||
+        ((startLen < endLen) && g.ops[1]) ||
+        ((startLen > endLen) && g.ops[2]))) {
+        logResult(4, null);
+        return g;
+    }
+
+    // Build graph if needed, then log search path
+    if (needsRebuild) {
+        const overlay = document.getElementById("overlay");
+        overlay.hidden = false;
+        g = await buildGraph(g.validWords, g.ops);
+        overlay.hidden = true;
+    }
+    const t = new WordTrace(g, startWord, endWord, g.ops);
+    logResult(BFS(t), t);
+    return g;
+}
+
+async function buildGraph(validWords, ops) {
+    const g = new WordGraph(validWords, ops);
+    for (const word of validWords) {
+        const v = new Vertex(word);
+        g.map.set(word, v);
+    }
+
+    return await buildLoop(g);
+}
+
+async function buildLoop(g) {
+    const progressBar = document.getElementById("progressbar");
+
+    let wordi = 0;
+    let interval = Math.floor(g.map.size / 100);
+    let percentage = 0;
+
+    for (const v of g.map.values()) {
+        v.adjacent = getAdjacent(v.word, g);
+
+        // If reached interval to update
+        if (wordi % interval == 0) {
+            progressBar.style.width = percentage + "%";
+            percentage++;
+            // Force style attribute to update
+            await new Promise(r => setTimeout(r, 1));
+        }
+        wordi++;
+    }
+
+    return g;
+}
+
 function getAdjacent(word, g) {
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
     const adjacent = new Set();
@@ -94,19 +192,6 @@ function BFS(t) {
 
 }
 
-async function main() {
-    const filename = "wordsets/words-58k.txt";
-    const validWords = await loadWords(filename);
-
-    let g = new WordGraph(validWords, null);
-
-    const submit = document.getElementById("submit");
-    submit.addEventListener("click", async function () {
-        g = await submitClick(g);
-    }
-    );
-}
-
 async function logResult(result, t) {
     const goal = document.getElementById("goal");
     const status = document.getElementById("status");
@@ -139,10 +224,7 @@ async function logResult(result, t) {
             return;
     }
 
-    // if (t == null) {
-    //     return;
-    // }
-
+    // Get list of word strings
     const trace = [];
     let current = t.graph.map.get(t.endWord);
     while (current != null) {
@@ -151,20 +233,18 @@ async function logResult(result, t) {
     }
     const nSteps = trace.length;
     const distance = nSteps - 1;
-    
     goal.innerText += ` (${distance})`;
 
-    console.log(nSteps);
-
-    const tracebox = document.getElementById("trace");
-
+    // Put words in trace box with definition alt text
+    const tracebox = document.getElementById("tracebox");
     for (let i = 0; i < nSteps; i++) {
         const step = document.createElement("div");
         step.className = "step";
         const word = trace.pop()
+
+        // API Call
         const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
         const data = await response.json();
-
         if (data.title == "No Definitions Found") {
             step.setAttribute("title", "No definition found.")
         } else {
@@ -173,124 +253,25 @@ async function logResult(result, t) {
             step.setAttribute("title", partOfSpeech + "\n" + definition);
         }
 
-        step.innerHTML = word;
+        step.innerText = word;
         tracebox.appendChild(step);
     }
 
+    // Only show output box when all definitions fetched
     outputbox.hidden = false;
 }
 
+async function main() {
+    const filename = "wordsets/words-58k.txt";
+    const validWords = await loadWords(filename);
 
-async function submitClick(g) {
-    const startWord = document.getElementById("start").value.toLowerCase();
-    const endWord = document.getElementById("end").value.toLowerCase();
-    
-    const outputbox = document.getElementById("outputbox");
-    outputbox.hidden = false;
-    const tracebox = document.getElementById("trace");
-    tracebox.innerHTML = "";
+    let g = new WordGraph(validWords, null);
 
-    const goal = document.getElementById("goal");
-    goal.innerText = `${startWord} \u2192 ${endWord}`;
-
-
-    // Check for new operations
-    const newops = [false, false, false];
-    if (document.getElementById("set").checked) {
-        newops[0] = true;
+    const submit = document.getElementById("submit");
+    submit.addEventListener("click", async function () {
+        g = await submitClick(g);
     }
-    if (document.getElementById("add").checked) {
-        newops[1] = true;
-    }
-    if (document.getElementById("remove").checked) {
-        newops[2] = true;
-    }
-
-    let needsRebuild = false;
-    // If nothing in map, or operations have changed
-    if (g.map.size == 0 || g.ops.toString() != newops.toString()) {
-        needsRebuild = true;
-        g.ops = newops;
-    }
-
-    if (!g.validWords.has(startWord)) {
-        logResult(2, null);
-        return g;
-    }
-    if (!g.validWords.has(endWord)) {
-        logResult(3, null);
-        return g;
-    }
-
-
-    startLen = startWord.length;
-    endLen = endWord.length;
-
-
-    // reverse this with demorgans laws
-    if (!((startWord == endWord) ||
-        ((startLen == endLen) && (g.ops[0] || (g.ops[1] && g.ops[2]))) ||
-        ((startLen < endLen) && g.ops[1]) ||
-        ((startLen > endLen) && g.ops[2]))) {
-        logResult(4, null);
-        return g;
-    }
-
-    outputbox.hidden = true;
-    if (needsRebuild) {
-        const overlay = document.getElementById("overlay");
-        overlay.hidden = false;
-        g = await buildGraph(g.validWords, g.ops);
-        overlay.hidden = true;
-    }
-
-    const t = new WordTrace(g, startWord, endWord, g.ops);
-    logResult(BFS(t), t);
-
-    return g;
+    );
 }
-
-async function buildGraph(validWords, ops) {
-
-
-
-    const g = new WordGraph(validWords, ops);
-
-
-    for (const word of validWords) {
-        const v = new Vertex(word);
-        g.map.set(word, v);
-    }
-
-    return await run(g);
-}
-
-
-
-
-async function run(g) {
-    const progressBar = document.getElementById("progressbar");
-
-    let i = 0;
-    let f = g.map.size;
-    let marker = Math.floor(f / 100);
-    let j = 0;
-
-    for (const v of g.map.values()) {
-        v.adjacent = getAdjacent(v.word, g);
-
-        if (i % marker == 0) {
-            progressBar.style.width = j + "%";
-            j++;
-            // force style attribute to update
-            await new Promise(r => setTimeout(r, 1));
-        }
-        i++;
-    }
-
-    return g;
-}
-
-
 
 main();
